5bd6caa6a857da7f3c6532bb8c4e7cca204054c0
OwitiKamau <arekama20@gmail.com>
2020-02-17 23:04:44 +0300

    test function added

---
+++ js/scripts.js
@@ -0,0 +1,12 @@
+var myAlert = function myFunction(){
+  var male= document.getElementById("male")
+  var female= document.getElementById("female")
+
+  if (male. checked==true) {
+    alert("male")
+  } else if (female. checked ==true) {
+    alert("female")
+  } else {
+    alert("input correct value")
+  }
+}
