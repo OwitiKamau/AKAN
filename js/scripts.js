40662991d76ff006bf9b909ef9597a5d7323fc90
OwitiKamau <arekama20@gmail.com>
2020-02-17 23:46:50 +0300

    get date function added

--- js/scripts.js
+++ js/scripts.js
@@ -4,9 +4,15 @@ var myAlert = function myFunction(){
   var date= document.getElementById("date").value
   var month= document.getElementById("month").value
   var year= document.getElementById("year").value
+  var d = new Date()
+  d .setDate(parseInt(date))
+  d .setMonth(parseInt(month) + 1)
+  d .setFullYear(parseInt(year))
 
-  if (year =="2002") {
-    alert("Good")
+  var day = d .getDay(0)
+
+  if (day = 1) {
+    alert("Monday")
   } else {
     alert("wrong")
   }
