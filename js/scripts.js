997a42fe9d014ab5c12ea797099fcc5cb263c73a
OwitiKamau <arekama20@gmail.com>
2020-02-18 00:36:56 +0300

    function complete and operational

--- js/scripts.js
+++ js/scripts.js
@@ -6,10 +6,21 @@ var myAlert = function myFunction(){
   var year= document.getElementById("year").value
   var d = new Date()
   d .setDate(parseInt(date))
-  d .setMonth(parseInt(month) + 1)
+  d .setMonth(parseInt(month)) 
   d .setFullYear(parseInt(year))
 
   var day = d .getDay(0)
 
-  
+  var akan1 = ["Kwasi", "Kwadwo","Kwabena","Kwaku","Yaw","Kofi","Kwame"]
+  var akan2 = ["Akosua","Adwoa","Abenaa","Akua","Yaa","Afua","Ama"]
+
+  if (male. checked ==true){
+    var name = ((akan1[day]) .toString())
+    alert(name)
+  } else if (female. checked ==true) {
+    var name = ((akan2[day]) .toString())
+    alert(name)
+  } else {
+    alert("Input Correct information")
+  }
 }
